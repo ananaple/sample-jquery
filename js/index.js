c4c4301049ac0881527b1462fd093c2912439831
Oleg Kuchma <rage.as.sage@gmail.com>
2016-09-01 16:49:45 +0000

    nothing is clear but its temporary

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(function() {
+    // $('div').html('Hello World!');
+    $('div').click(function() {
+        $(this).hide(2000);
+    }
+                    // setInterval(clock, 1000);
+        
+    });
+    
+    // });
+                    // function clock() {
+                    // $('body').html((new Date).toLocaleTimeString())
+                    // }
